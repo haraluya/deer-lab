a56b9934b8595647f654e80856cff27d951c85f7
haraluya <haraluya777@gmail.com>
2025-08-26 19:16:42 +0800

    feat: Migrate to Next.js server deployment on Cloud Functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,23 @@
 // functions/src/index.ts
 import { initializeApp } from "firebase-admin/app";
 import { onRequest } from "firebase-functions/v2/https";
+import next from "next";
+import path from "path";
 
 // 初始化 Firebase Admin SDK，只需一次
 initializeApp();
+
+// 初始化 Next.js App
+const nextApp = next({
+  dev: false,
+  conf: { distDir: path.join("..", ".next") },
+});
+const nextHandle = nextApp.getRequestHandler();
+
+// 建立 nextServer 雲端函數
+export const nextServer = onRequest({ maxInstances: 10 }, (req, res) => {
+  return nextApp.prepare().then(() => nextHandle(req, res));
+});
 
 // 創建一個簡單的測試函數
 export const testFunction = onRequest((request, response) => {
